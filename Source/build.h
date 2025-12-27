90863912199aef402d70de658c11796337f61c22
ganzziani <gabriel@gabotronics.com>
2025-12-26 22:42:36 -0500

    lcd_line for inverted display option

--- Source/build.h
+++ Source/build.h
@@ -1,8 +1,8 @@
-#define BUILD_NUMBER 2125 
+#define BUILD_NUMBER 2129 
 #define BUILD_YEAR   25
 #define BUILD_MONTH  12 
 #define BUILD_DAY    26 
-#define BUILD_HOUR   21 
-#define BUILD_MINUTE 57 
-#define BUILD_SECOND 11 
+#define BUILD_HOUR   22 
+#define BUILD_MINUTE 41 
+#define BUILD_SECOND 41 
 // This file is generated from build_inc.bat
