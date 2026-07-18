b5fd9864002de9539f9c7ad393d5975eeb66ba7d
Gabriel Anzziani <gabriel@gabotronics.com>
2026-07-18 19:33:09 -0400

    Fixed moon icon with the inverted graphics

--- Source/build.h
+++ Source/build.h
@@ -1,8 +1,8 @@
-#define BUILD_NUMBER 4028 
+#define BUILD_NUMBER 4030 
 #define BUILD_YEAR   26
 #define BUILD_MONTH  7 
 #define BUILD_DAY    18 
-#define BUILD_HOUR   17 
-#define BUILD_MINUTE 58 
-#define BUILD_SECOND 12 
+#define BUILD_HOUR   19 
+#define BUILD_MINUTE 30 
+#define BUILD_SECOND 25 
 // This file is generated from build_inc.bat
